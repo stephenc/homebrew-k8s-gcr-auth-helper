c6b769f91e05fe0ac6e77905b918ea977b7568b7
Stephen Connolly <stephen.alan.connolly@gmail.com>
2020-03-27 15:33:45 +0000

    k8s-gcr-auth-helper 0.1.4

--- Formula/k8s-gcr-auth-helper.rb
+++ Formula/k8s-gcr-auth-helper.rb
@@ -4,9 +4,9 @@
 class K8sGcrAuthHelper < Formula
   desc "A Kubernetes authentication helper to expose per-user credentials as Image Pull Secrets for Google Container Registry"
   homepage "https://github.com/stephenc/k8s-gcr-auth-helper"
-  url "https://github.com/stephenc/k8s-gcr-auth-helper/releases/download/v0.1.3/k8s-gcr-auth-helper-x86_64-apple-macos.tar.gz"
-  sha256 "9f5e3921426f0adbedd98f336d5a03b8e5914c1bf7341d7b63acc24c5a2f4af8"
-  version "0.1.3"
+  url "https://github.com/stephenc/k8s-gcr-auth-helper/releases/download/v0.1.4/k8s-gcr-auth-helper-x86_64-apple-macos.tar.gz"
+  sha256 "e4d80d0049f9ae80482f3dafc9ebd8bf6581512125675738bde5cc05b404aff4"
+  version "0.1.4"
 
   def install
     bin.install "k8s-gcr-auth-helper"
